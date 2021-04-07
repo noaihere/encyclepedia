d06c176d46174ff5a8eb6f12a888ff847b8f6505
noaihere <44403965+noaihere@users.noreply.github.com>
2021-04-06 19:02:42 -0700

    add category

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,7 @@ class BlogIndex extends React.Component {
           const title = get(node, 'title') || node.slug
           return (
             <div key={node.slug}>
+            <h3>node.metadata.category</h3>
               <h3
                 style={{
                   marginBottom: rhythm(1 / 4),
@@ -57,6 +58,7 @@ export const pageQuery = graphql`
         node {
           metadata {
             description
+            category
           }
           slug
           title
